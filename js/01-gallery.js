d36e059c241c0712a20ca50ecf2d0e19395bada8
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-02-17 14:37:42 +0100

    tasks hw-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector(".gallery");
+const imagesEl = createGalleryImg(galleryItems);
+galleryContainerEl.insertAdjacentHTML("beforeend", imagesEl);
+
+
+function createGalleryImg(item) {
+    return galleryItems.map(({ preview, original, description }
+    
+        ) => {
+           return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+        }).join('');
+};
+
+
+const onContainerClick = (e) => {
+    e.preventDefault();
+
+
+  const source = e.target.dataset.source;
+    if (!source) return;
+const instance = basicLightbox.create(`
+    <img src="${source}"width="800" height="600">`);
+
+  instance.show();
+};
+galleryContainerEl.addEventListener("click", onContainerClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryContainerEl = document.querySelector('.gallery');
+const imagesEl = createGalleryImg(galleryItems);
+galleryContainerEl.insertAdjacentHTML('beforeend', imagesEl);
+
+function createGalleryImg(item) {
+    return galleryItems.map(({ preview, original, description }) => {
+       return `<ul class="gallery">
+        <li>
+ <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>
+</ul>`
+    }).join('');
+
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {  
+captionsData: "alt",
+captionDelay: 250,
+captionType: "alt",
+  });
